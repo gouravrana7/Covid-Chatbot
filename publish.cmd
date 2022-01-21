310cdb03b63e352fb776dc9b486645ac1c3facdb
Gourav Rana <82229078+gouravrana7@users.noreply.github.com>
2022-01-22 00:02:53 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnamaker7015-bot-b31f-Web-Deploy.pubxml -p:Password=Ffht2jtBebYbWHFP6lnH12wDpcaDJADo3hQL7SJ78ZTYL3JyH7NmxwbEdC2u
+
